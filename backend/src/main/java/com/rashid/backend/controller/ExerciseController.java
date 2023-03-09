ffb8032dc31a1ed92d419929dd134470dcfea2b8
Alli Rashid <allirashid97@gmail.com>
2023-03-09 16:10:49 -0400

    added post method

--- backend/src/main/java/com/rashid/backend/controller/ExerciseController.java
+++ backend/src/main/java/com/rashid/backend/controller/ExerciseController.java
@@ -3,10 +3,9 @@ package com.rashid.backend.controller;
 import com.rashid.backend.Exercise;
 import com.rashid.backend.ExerciseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,8 +14,28 @@ public class ExerciseController {
     @Autowired
     private ExerciseRepository exRepo;
 
+    public ExerciseController(ExerciseRepository exRepo) {
+        this.exRepo = exRepo;
+    }
+
     @GetMapping
     public List<Exercise> getExercises() {
         return exRepo.findAll();
     }
+
+    record NewExerciseRequest(
+            String exName,
+            ArrayList<String> muscleGroup
+    ) {
+
+    }
+
+    @PostMapping
+    public void addExercise(@RequestBody NewExerciseRequest request) {
+        Exercise exercise = new Exercise();
+        exercise.setExName(request.exName);
+        exercise.setMuscleGroup(request.muscleGroup);
+        exRepo.save(exercise);
+    }
+
 }
